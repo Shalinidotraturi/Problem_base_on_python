d15a439ec3bd37e68506b8f15df08ce989dec785
Shalini <99462415+Shalinidotraturi@users.noreply.github.com>
2022-02-17 22:22:52 -0800

    Add files via upload

---
+++ Input_output_program.py
@@ -0,0 +1,36 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+no1=input("Enter no:")
+Enter no:30
+no1
+'30'
+no2=input("Enter no:")
+Enter no:2
+no2
+'2'
+no1+no2
+'302'
+no1=int(input("Enter no:"))
+Enter no:30
+no1
+30
+no2=float(input("Enter no:"))
+Enter no:2.3
+no3=bool(input("Enter no:"))
+Enter no:True
+no3
+True
+no4=complex(input("Enter no:"))
+Enter no:2+3j
+no4
+(2+3j)
+name=input("Enter name")
+Enter namesugan
+name
+'sugan'
+print("Welcome")
+Welcome
+age=20
+print("Myage:",age)
+Myage: 20
+
